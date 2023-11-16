94d45dbca71f332ad4e98b00a7eb3dec2aac0cd5
sungsoo <142865231+sslee1210@users.noreply.github.com>
2023-11-17 01:03:15 +0900

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -60,6 +60,60 @@ class App extends Component {
       </TodoListTemplate>
     );
   }
+
+  handleToggle = (id) => {
+    const { todos } = this.state;
+    const index = todos.findIndex((todo) => todo.id === id);
+
+    const selected = todos[index];
+
+    this.setState({
+      todos: [
+        ...todos.slice(0, index),
+        {
+          ...selected,
+          checked: !selected.checked,
+        },
+        ...todos.slice(index + 1, todos.length),
+      ],
+    });
+  };
+  handleRemove = (id) => {
+    const { todos } = this.state;
+    this.setState({
+      todos: todos.filter((todo) => todo.id !== id),
+    });
+  };
+
+  render() {
+    const { input, todos } = this.state;
+    const {
+      handleChange,
+      handleCreate,
+      handleKeyPress,
+      handleToggle,
+      handleRemove,
+    } = this;
+
+    return (
+      <TodoListTemplate
+        form={
+          <Form
+            value={input}
+            onKeyPress={handleKeyPress}
+            onChange={handleChange}
+            onCreate={handleCreate}
+          />
+        }
+      >
+        <TodoItemList
+          todos={todos}
+          onToggle={handleToggle}
+          onRemove={handleRemove}
+        />
+      </TodoListTemplate>
+    );
+  }
 }
 
 export default App;
--- src/components/TodoItemList.js
+++ src/components/TodoItemList.js
@@ -4,10 +4,6 @@ import TodoItem from "./TodoItem";
 class TodoItemList extends Component {
   render() {
     const { todos, onToggle, onRemove } = this.props;
-    // 배열안의 원소를 모두 제곱하기
-    const numbers = [1, 2, 3, 4, 5];
-    const squared = numbers.map((number) => number * number);
-    console.log(numbers); // [1,4,9,16,25]
 
     const todoList = todos.map(({ id, text, checked }) => (
       <TodoItem
